fb8f3e490a62eef2d28a6e649a11746bb9b4e8ac
Jan Taro Svejda <jan.svejda@uni-due.de>
2022-07-05 14:49:44 +0200

    simplification for arbitrary solid names

--- split_stl_by_solids.py
+++ split_stl_by_solids.py
@@ -16,17 +16,16 @@ def split_stl_by_solids(filename, create_dir=True):
 
     solid_lines = []
     for line in lines:
-        match_solid_start = re.fullmatch(r'solid component([0-9_]+):solid([0-9_]+)\n', line)
-        match_solid_stop = re.fullmatch(r'endsolid component([0-9_]+):solid([0-9_]+)\n', line)
+        match_solid_start = re.match(r'solid component(.+)\n', line)
+        match_solid_stop = re.match(r'endsolid component(.+)', line)
         if match_solid_start is not None:
             print(match_solid_start.group(0))
             solid_lines = []
             solid_lines.append(line)
-            component_no = match_solid_start.group(1)
-            solid_no = match_solid_start.group(2)
+            solid_name = match_solid_start.group(1)
         elif match_solid_stop is not None:
             solid_lines.append(line)
-            solid_filename = os.path.join(directory, filename_splited[0] + '_comp{:s}'.format(component_no) + '_solid{:s}'.format(solid_no) + filename_splited[1] )
+            solid_filename = os.path.join(directory, filename_splited[0] + '_component{:s}'.format(solid_name) + filename_splited[1] )
             with open(solid_filename, 'w') as file:
                 for solid_line in solid_lines:
                     file.write('{:s}'.format(solid_line))
